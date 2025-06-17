461038954d08c5b913e134f3f9ec71fc5f4f3442
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-06-17 22:13:00 +0200

    keyword result and state change

--- src/backend/events/keyword_result.gd
+++ src/backend/events/keyword_result.gd
@@ -2,9 +2,32 @@ class_name KeywordResult
 extends Resource
 
 var keyword: String
-var args: Array[Variant] = []
 
-var atoms_created: Array[int] = []
 var state_changes: Array[StateChange] = []
 
 var sub_results: Array[KeywordResult] = []
+
+func add_result(result: KeywordResult) -> KeywordResult:
+    sub_results.append(result)
+    return self
+
+func add_state_change(change: StateChange) -> KeywordResult:
+    state_changes.append(change)
+    return self
+
+func _init(keyword_: String) -> void:
+    keyword = keyword_
+
+static func noop(keyword_: String) -> KeywordResult:
+    var result = KeywordResult.new(keyword_)
+    return result
+
+static func composite(keyword_: String, sub_results_: Array[KeywordResult]) -> KeywordResult:
+    var result = KeywordResult.new(keyword_)
+    result.sub_results = sub_results_
+    return result
+
+static func from_changes(keyword_: String, changes_: Array[StateChange]) -> KeywordResult:
+    var result = KeywordResult.new(keyword_)
+    result.state_changes = changes_
+    return result
--- src/backend/events/state_change.gd
+++ src/backend/events/state_change.gd
@@ -1,7 +1,29 @@
 class_name StateChange
 extends Resource
 
+var was_noop: bool
+
 var atom_id: int
+var atom_created: bool
+
 var property_name: String
 var value_before: Variant
 var value_after: Variant
+
+static func created(atom: Atom) -> StateChange:
+    var change = StateChange.new()
+    change.atom_id = atom.id
+    change.atom_created = true
+    return change
+
+static func changed(atom: Atom, property_name_: String, value_before_: Variant) -> StateChange:
+    var change = StateChange.new()
+    change.atom_id = atom.id
+    change.atom_created = false
+    change.property_name = property_name_
+    change.value_before = value_before_
+    change.value_after = atom.get(property_name_)
+
+    change.was_noop = (change.value_before == change.value_after)
+
+    return change
