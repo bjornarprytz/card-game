1ee56f8758ceaaa58e4458ccaf57003ab1fadfa1
Bjørnar Prytz <bjornarprytz@gmail.com>
2025-07-16 21:33:16 +0200

    thinking

--- src/game_loop/actions/play_card_action.gd
+++ src/game_loop/actions/play_card_action.gd
@@ -8,24 +8,27 @@ var _effects: Array[EffectProto]
 var _effect_index: int = 0
 
 func _init(play_card_context: PlayCardContext) -> void:
-	action_type = "play_card"
-	assert(play_card_context != null, "PlayCardContext cannot be null")
-	_context = play_card_context
-	_effects = _context.card.card_data.effects
+    action_type = "play_card"
+    assert(play_card_context != null, "PlayCardContext cannot be null")
+    _context = play_card_context
+    _effects = _context.card.card_data.effects
 
 func has_next_keyword() -> bool: # Check if there are more keywords to process
-	return _effect_index < _effects.size()
+    return _effect_index < _effects.size()
 
 func next_keyword() -> KeywordNode:
-	var effect_proto = _effects[_effect_index]
-	_effect_index += 1
-	
-	if not effect_proto.evaluate_condition(_context):
-		return KeywordNode.noop(effect_proto.keyword)
-	
-	var args = effect_proto.resolve_args(_context)
-	return Keywords.create_operation_tree(effect_proto.keyword, args)
+    var effect_proto = _effects[_effect_index]
+    _effect_index += 1
+
+    ## TODO: Maybe the prompt is detected and returned here. 
+    ## TODO: The next keyword (e.g. discard_cards(atoms)) would need the prompt response to be passed in.
+    
+    if not effect_proto.evaluate_condition(_context):
+        return KeywordNode.noop(effect_proto.keyword)
+    
+    var args = effect_proto.resolve_args(_context)
+    return Keywords.create_operation_tree(effect_proto.keyword, args)
 
 func _to_string() -> String:
-	# Returns a string representation of the action
-	return "PlayCardAction: %s" % _context.card
+    # Returns a string representation of the action
+    return "PlayCardAction: %s" % _context.card
--- src/game_loop/prompts/prompt.gd
+++ src/game_loop/prompts/prompt.gd
@@ -4,5 +4,3 @@ extends Resource
 func validate_action(_action: GameAction) -> bool:
     push_error("validate_action not implemented in base Prompt class")
     return false
-
-## TODO: Could it be a good idea for the prompt to create the effect block? For game actions, it would just pass the action through, but for othe prompts it could spare 
